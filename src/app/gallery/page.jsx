217377a603ddff7754c6d4ccf2b5517ff5e22708
TI Sabbir <tisabbir0@gmail.com>
2025-02-11 08:32:02 +0600

    add : image loaded dynamically

--- src/app/gallery/page.jsx
+++ src/app/gallery/page.jsx
@@ -5,8 +5,12 @@ const GalleryPage = () => {
     return (
         <div>
             This is the gallery we want to visit every vacation
+
+           {
+             [2,3,4,5].map((img)=> <Image key={"img"} src={`/${img}.png`} alt="Image" width={500} height={500} />)
+           }
             
-            <Image src="/2.png" alt="Image" width={500} height={500} />
+            {/* <Image src="/2.png" alt="Image" width={500} height={500} /> */}
         </div>
     );
 };
