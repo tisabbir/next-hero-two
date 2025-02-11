b4cf1e771ff43f59bc519f21a002ba08468333fc
TI Sabbir <tisabbir0@gmail.com>
2025-02-11 10:18:31 +0600

    add  : loading page is added

---
+++ src/app/posts/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const loading = () => {
+    return (
+        <div>
+            Post is being loaded. Please wait a few hours bro....
+        </div>
+    );
+};
+
+export default loading;
