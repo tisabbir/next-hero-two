9e133cc22413b8c45f2fa4e304f581a87f45a6ed
TI Sabbir <tisabbir0@gmail.com>
2025-02-13 22:45:51 +0600

    add : env variables

--- src/services/postsAPI.js
+++ src/services/postsAPI.js
@@ -1,7 +1,7 @@
 import { redirect } from "next/dist/server/api-utils";
 
 export const getPosts = async () => {
-    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_ROUTE}/posts`);
     const data = res.json();
 
     return data;
